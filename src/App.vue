8376b0f300aa91f00cbd551315c9d5e99ea10355
Thomas Baudon <thomas.baudon@pokeet.fr>
2018-10-17 00:45:33 +0200

    adding bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-navbar toggleable="md" type="dark" variant="primary">
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+      <b-navbar-brand href="#">Headless-client</b-navbar-brand>
+    </b-navbar>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col">
+          <HelloWorld v-if="loggedIn"/>
+          <Login v-if="!loggedIn"/>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Login from './components/Login.vue'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
+    Login,
     HelloWorld
+  },
+  data: () => {
+    return {
+      loggedIn: false
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <b-card>
+    login
+  </b-card>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style scoped>
+
+</style>
